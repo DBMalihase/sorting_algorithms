fc96a642e5b4950500456e1540067a7f2e74d3f3
DBMalihase <dennismalihase@gmail.com>
2023-01-21 09:19:22 -0800

    Sorting algorithms and Big O

--- sort.h
+++ sort.h
@@ -31,7 +31,7 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-} listint_t
+} listint_t;
 
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
